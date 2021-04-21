dd9ca32b598c5ac7139bc20e105eeb0663217163
vlrosa-dev <victor.lmr@hotmail.com>
2021-04-21 15:43:11 -0300

    Seller Entity Class

--- src/application/App.java
+++ src/application/App.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model_entities.Department;
+import model_entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
         Department obj = new Department(2, "Books");
 
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Victor", "victor@gmail.com", new Date(), 2000.0, obj);
+
+        System.out.println(seller);
     }
 }
